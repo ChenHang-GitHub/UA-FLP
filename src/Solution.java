b512a84ee6c2a9704799ffd2fb5c874c4ea9d622
ChenHang-GitHub <704635857@qq.com>
2020-04-06 15:43:31 +0800

    csh

---
+++ src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+/**
+ * @ClassName: Solution
+ * @author: csh
+ * @Description:
+ */
+public class Solution {
+
+    private  Problems problems;
+    private  static Random rand= new Random();
+
+    public Solution(boolean initial) {
+        problems =Problems.getProblem();
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
